4f8c40c7bd463d938e96a2cb72d78491ff1b7cfe
Corey Nadeau <123282786+corey-nadeau@users.noreply.github.com>
2023-03-27 14:15:49 -0500

    Delete firebase.config.ts

--- src/config/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC5klpW907d4e666HwT0L3_p1YCKEwjf4k",
-  authDomain: "react-car-inv.firebaseapp.com",
-  projectId: "react-car-inv",
-  storageBucket: "react-car-inv.appspot.com",
-  messagingSenderId: "275256959499",
-  appId: "1:275256959499:web:cefe8e1d7ffab182915d0e"
-};
